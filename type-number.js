9403b6fe24ae74ecc62da651a18e8c34164389c1
Paloma Faria <54600137+palomafaria@users.noreply.github.com>
2022-12-29 16:34:25 -0300

    Tipos Primitivos

---
+++ type-number.js
@@ -0,0 +1,19 @@
+//Tipo Number
+const meuNumero = 3;
+
+const primeiroNumero = 5;
+const segundoNumero = 7;
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+console.log("Resultado da operação: " + operacaoMatematica);
+
+//ponto flutuante
+const numeroPontoFlutuante = 3.3;
+const pontoFlutanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+console.log("Operação ponto flutuante: " + novaOperacao);
+
+//NaN -> Not a number
+const alura = "alura";
+console.log("Not a number: " + alura * primeiroNumero);
---
+++ type-string.js
@@ -0,0 +1,22 @@
+const texto1 = "Olá Mundo!";
+const texto2 = "Olá Mundo!";
+const senha = "senhaSuperSegura456!";
+const texto3 = "567";
+
+const citacao = "Meu nome é ";
+console.log(citacao);
+
+//Template String
+const templateString = `Essa é uma template string ${texto1}`;
+console.log(templateString);
+
+//Concatenação
+const nome = "Paloma";
+console.log(citacao + nome);
+
+//Unicode
+const cifrao = "\u0024";
+const aMaiusculo = "\u0041";
+const tique = "\u2705";
+const hiragana = "\u3041";
+console.log(cifrao, aMaiusculo, tique, hiragana);
